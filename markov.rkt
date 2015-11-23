a5d0aa49cad0ce78fe7ad9bb74af7f0670c19bcf
David Fluck <fluck.d@gmail.com>
2015-11-22 20:35:33 -0500

    Created (get-words) function
    (get-words) takes a number of elements to have in each list, an empty
    starting list, and a line reader, and prints out a list of num-elems
    elements at a time.

--- markov.rkt
+++ markov.rkt
@@ -1,5 +1,7 @@
 #lang racket
-; Return a function that reads one line at a time from a file.
+(require racket/generator)
+
+; Create a closure that reads one line at a time from a file.
 (define (make-line-reader filename)
   (let ([file (open-input-file filename)])
     (lambda ()
@@ -8,3 +10,31 @@
           (if (eof-object? next-line)
               (close-input-port file)
             next-line))))))
+
+; Create a closure that returns a certain number of "words" from a file.
+(define (make-line-collector filename num-elems)
+  (let ([line-reader (make-line-reader filename)]
+        [word-list '()])
+    (lambda ()
+      (cond
+        [(< (length word-list) num-elems)
+         (let ([next-line (line-reader)])
+            (if (not (void? next-line))
+                (set! word-list (append word-list (string-split next-line)))
+              (void)))]
+        [else
+         (let ([ret-list (take word-list num-elems)])
+           (begin
+             (set! word-list (drop word-list num-elems))
+             ret-list))]))))
+
+(define (get-words num-elems buffer line-reader)
+  (if (< (length buffer) num-elems)
+      (let ([next-line (line-reader)])
+        (if (void? next-line)
+            (void)
+          (get-words num-elems (append buffer (string-split next-line)) line-reader)))
+    (begin
+      (print (take buffer num-elems))
+      (get-words num-elems (drop buffer num-elems) line-reader))))
+      
